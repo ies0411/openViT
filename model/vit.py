dc3a0608b6d335d9d3404b53e6060169ced136a4
ies0411 <ies041196@gmail.com>
2023-12-18 22:38:11 +0900

    update

--- model/vit.py
+++ model/vit.py
@@ -55,42 +55,39 @@ class EmbeddingMudule(
 
 
 class MultiheadedSelfAttention(nn.Module):
-    def __init__(self, latent_vec_dim, num_heads, drop_rate):
+    def __init__(self, latent_vector_dim, head_num, drop_rate):
         super().__init__()
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.num_heads = num_heads
-        self.latent_vec_dim = latent_vec_dim
-        self.head_dim = int(latent_vec_dim / num_heads)
-        self.query = nn.Linear(latent_vec_dim, latent_vec_dim)
-        self.key = nn.Linear(latent_vec_dim, latent_vec_dim)
-        self.value = nn.Linear(latent_vec_dim, latent_vec_dim)
-        self.scale = torch.sqrt(self.head_dim * torch.ones(1)).to(device)
+        self.latent_vector_dim = latent_vector_dim
+        self.head_num = head_num
+        self.query = nn.Linear(latent_vector_dim, latent_vector_dim)
+        self.key = nn.Linear(latent_vector_dim, latent_vector_dim)
+        self.value = nn.Linear(latent_vector_dim, latent_vector_dim)
         self.dropout = nn.Dropout(drop_rate)
 
     def forward(self, x):
+        q = self.query(x)  # toal head에 한 query
+        k = self.key(x)  # toal head에 한 key
+        v = self.value(x)  # toal head에 한 value
+        q = q.view(x.size(0), self.head_num, -1, self.head_dim)
+        k = k.view(x.size(0), self.head_num, self.head_dim, -1)
+        v = v.view(x.size(0), self.head_num, -1, self.head_dim)
+        att = torch.softmax(
+            q @ k / torch.sqrt(self.head_dim * torch.ones(1)).to(device), dim=-1
+        )
         batch_size = x.size(0)
-        q = self.query(x)
-        k = self.key(x)
-        v = self.value(x)
-        q = q.view(batch_size, -1, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
-        k = k.view(batch_size, -1, self.num_heads, self.head_dim).permute(
-            0, 2, 3, 1
-        )  # k.t
-        v = v.view(batch_size, -1, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
-        attention = torch.softmax(q @ k / self.scale, dim=-1)
         x = self.dropout(attention) @ v
-        x = x.permute(0, 2, 1, 3).reshape(batch_size, -1, self.latent_vec_dim)
-
-        return x, attention
+        x = x.reshape(batch_size, -1, self.latent_vector_dim)
 
 
 class TransformerEncoder(nn.Module):
-    def __init__(self, latent_vec_dim, num_heads, mlp_hidden_dim, drop_rate):
+    def __init__(self, latent_vector_dim, head_num, mlp_hidden_dim, drop_rate=0.1):
         super().__init__()
-        self.ln1 = nn.LayerNorm(latent_vec_dim)
-        self.ln2 = nn.LayerNorm(latent_vec_dim)
+        self.ln1 = nn.LayerNorm(latent_vector_dim)
+        self.ln2 = nn.LayerNorm(latent_vector_dim)
         self.msa = MultiheadedSelfAttention(
-            latent_vec_dim=latent_vec_dim, num_heads=num_heads, drop_rate=drop_rate
+            latent_vector_dim=latent_vector_dim,
+            head_num=head_num,
+            drop_rate=drop_rate,
         )
         self.dropout = nn.Dropout(drop_rate)
         self.mlp = nn.Sequential(
